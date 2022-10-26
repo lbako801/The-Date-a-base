4e1a6dddf38829173fb2235dfb44f8245a9bc874
lbako801 <lbako98@gmail.com>
2022-10-25 19:27:42 -0600

    merges

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,20 @@
 // These variables grab the form element from the HTML
 var genreChoiceEl = document.getElementById('moviegenres');
+
 var drinkChoiceEl = document.getElementById('drinks');
+
 var cuisineChoiceEl = document.getElementById('cuisines');
 
 var choicesEl = document.getElementById('choices');
 
-// This function below conjsole logs the user's dropdown choices.
+// This function below console logs the user's dropdown choices.
 choicesEl.addEventListener("submit", function (event) {
     event.preventDefault();
     console.log(genreChoiceEl.value);
     console.log(drinkChoiceEl.value);
     console.log(cuisineChoiceEl.value);
 });
+
+
+
+
